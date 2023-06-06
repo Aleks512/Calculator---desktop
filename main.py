8d0e8c410a9df5105ef3300c7c40ff854be62db6
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-06-06 15:25:32 +0200

    get_buttons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QLabel, \
-    QGridLayout
+    QGridLayout, QLineEdit
 
 BUTTONS = {
     "C": (1, 0, 1, 1),
@@ -20,14 +20,31 @@ BUTTONS = {
     "0": (5, 0, 1, 2),
     ".": (5, 2, 1, 1),
     "=": (5, 3, 1, 1),
-},
+}
 
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.buttons = {}
         self.setWindowTitle('Super Calculatrice')
-        main_layout = QGridLayout(self)
+        # set up main layout and fix it to window
+        self.main_layout = QGridLayout(self)
+
+        # set up input and result window
+        self.result_and_adit = QLineEdit("0")
+
+        # add it to main layout with coordinates
+        self.main_layout.addWidget(self.result_and_adit, 0, 0, 1, 4)
+
+        # create all BUTTONS
+
+        for key, value in BUTTONS.items():
+            button = QPushButton(key)
+            self.main_layout.addWidget(button, *value) #tupple unpacking
+            # create dico of buttons to connect them to slots
+            self.buttons[key] = button
+
+
 
 
 
